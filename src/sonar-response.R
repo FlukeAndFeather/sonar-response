b494ae2c2c6acfeaaa5b70d7bf6fdda7606cd72b
Max Czapanskiy <maxczap@stanford.edu>
2019-04-26 09:41:47 -0700

    Calculate displacement threshold

--- src/sonar-response.R
+++ src/sonar-response.R
@@ -134,12 +134,12 @@ Esonar_fun <- function(td_min, tf_min) {
 ## Behavioral responses
 scenario_tbl <- tribble(
   ~binomial,                 ~t_d_min, ~t_f_min, ~U_f_ms,
-  "Phocoena phocoena",       60,       60,       5,
-  "Grampus griseus",         60,       60,       5,
-  "Ziphius cavirostris",     60,       60,       5,
-  "Physeter macrocephalus",  60,       60,       5,
-  "Megaptera novaeangliae",  60,       60,       5,
-  "Balaenoptera musculus",   60,       60,       5
+  "Phocoena phocoena",       60,       30,       5,
+  "Grampus griseus",         60,       30,       5,
+  "Ziphius cavirostris",     60,       30,       5,
+  "Physeter macrocephalus",  60,       30,       5,
+  "Megaptera novaeangliae",  60,       30,       5,
+  "Balaenoptera musculus",   60,       30,       5
 ) %>% 
   mutate(binomial = factor(binomial, levels = binom_levels)) %>% 
   ## Morphologies
@@ -168,7 +168,8 @@ Esonar_tbl <- scenario_tbl %>%
     delta_ff <- ff_flight - ff_basal
     ff_shape <- 4
     ff_scale <- delta_ff / ff_shape
-    CL <- 1.46 + 0.0005 * data$Mass_kg
+    # squared to get units of J/stroke
+    CL <- 1.46 + 0.0005 * data$Mass_kg^2
     CL_shape <- 4
     CL_scale <- CL / CL_shape
     q_arg <- list(rf_h = list(),
@@ -207,7 +208,7 @@ Esonar_tbl <- scenario_tbl %>%
                     args = q_arg[[4]]) +
       geom_vline(xintercept = CL,
                  linetype = "dashed") +
-      labs(x = "Locomotor cost (J/kg/stroke)",
+      labs(x = "Locomotor cost (J/stroke)",
            y = "density",
            title = key$binomial) +
       theme_minimal()
@@ -273,7 +274,7 @@ Esonar_tbl <- scenario_tbl %>%
     esonar_pval <- summary(esonar_linear)$coefficients[,4]
     esonar_ci <- as_tibble(confint(esonar_linear, level = 0.95),
                            rownames = "param")
-    colnames(esonar_ci)[2:3] <- c(ci_min, ci_max)
+    colnames(esonar_ci)[2:3] <- c("ci_min", "ci_max")
       
     # Combine and drop info for the intercept
     lm_results <- cbind(esonar_ci, esonar_coef, esonar_pval)[-1,]
@@ -284,9 +285,11 @@ Esonar_tbl <- scenario_tbl %>%
                                 iqr_Esonar = IQR(Esonar_kJ),
                                 median_inout = median(inout_ratio),
                                 inout_25 = quantile(inout_ratio, 0.25),
-                                inout_75 = quantile(inout_ratio, 0.75))
+                                inout_75 = quantile(inout_ratio, 0.75),
+                                mean_inout = mean(inout_ratio),
+                                se_inout = sd(inout_ratio)/sqrt(n()))
     
-    cbind(lm_results, esonar_results, param_args)
+     
   })
 
 # A plot with the normalized linear model coefficients
@@ -300,15 +303,18 @@ coef_data <- Esonar_tbl %>%
                                    "Delta*f[f]",
                                    "E[p]",
                                    "r[f]")))
-ggplot(coef_data, aes(x = binomial, y = esonar_coef, group = binomial)) +
+# Color palette: Color Brewer Set1. Six classes, dropping the yellow.
+pal <- RColorBrewer::brewer.pal(7, "Set1")[-6]
+ggplot(coef_data, aes(x = binomial, y = esonar_coef, color = binomial)) +
   geom_hline(aes(yintercept = mean_coef),
              summarize(group_by(coef_data, param), 
                        mean_coef = mean(esonar_coef)),
              linetype = "dashed") +
   geom_point(position = position_dodge(width = 0.4)) +
-  geom_errorbar(aes(ymin = `2.5 %`, ymax = `97.5 %`), 
+  geom_errorbar(aes(ymin = ci_min, ymax = ci_max), 
                 width = 0.2, 
                 position = position_dodge(width = 0.4)) +
+  scale_color_manual(values = pal) +
   facet_grid(~ param,
              labeller = label_parsed,
              switch = "x") +
@@ -318,213 +324,87 @@ ggplot(coef_data, aes(x = binomial, y = esonar_coef, group = binomial)) +
   theme(axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
         axis.title.x = element_blank(),
-        legend.position = "none",
+        legend.position = "bottom",
+        legend.title = element_blank(),
         strip.background = element_blank(),
         strip.placement = "outside")
 ggsave("figs/extreme_flight_coefs.pdf",
        width = 9,
        height = 6)
 
-# Ratio of energy in to energy out
-ggplot(Esonar_tbl, aes(binomial, median_inout)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = inout_25+1, ymax = inout_75),
-                width = 0.2) +
-  scale_y_log10(labels = log_labels) +
-  scale_x_discrete(labels = function(lbl) str_replace(lbl, " ", "\n")) +
-  labs(y = expression(E["in"]:E[out])) +
-  theme_classic() +
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1))
-
-# Case studies ----------------------------------------------------------------
-casestudy_tbl <- tribble(
-  ~binomial,                 ~t_d_min, ~t_f_min, ~U_f_ms,
-  "Ziphius cavirostris",     6 * 60,   30,       4.5,
-  "Balaenoptera musculus",   1 * 60,   5,        3
-) %>% 
-  mutate(binomial = factor(binomial, levels = binom_levels)) %>% 
-  ## Morphologies
-  left_join(select(morphologies,
-                   binomial,
-                   Mass_kg,
-                   Length_m),
-            by = "binomial") %>%
-  ## rf probabilities
-  left_join(select(rf_tbl, binomial, q_rf_fun), by = "binomial") %>% 
-  ## Ep probabilities
-  left_join(select(Ep_tbl, binomial, q_Ep_fun), by = "binomial") 
-
-Esonar_tbl2 <- casestudy_tbl %>% 
+# A table of the ratio of energy in to energy out
+Esonar_tbl %>% 
   group_by(binomial) %>% 
-  group_map(function(data, key) {
-    param <- c("rf_h", "Ep_kJ", "delta_ff", "CL")
-    q <- list(rf_h = data$q_rf_fun[[1]],
-              Ep_kJ = data$q_Ep_fun[[1]],
-              delta_ff = qgamma,
-              CL = qgamma)
+  slice(1) %>% 
+  ungroup %>% 
+  select(binomial, mean_inout, se_inout) %>% 
+  write_csv("figs/extreme_flight_inout.csv")
+
+# Critical thresholds ---------------------------------------------------------
+scenario_tbl <- tribble(
+  ~scenario,        ~t_f_min, ~U_f_ms,
+  "no_flight",      0,        0,
+  "weak_flight",    5,        2.5,
+  "strong_flight",  15,       3.5,
+  "extreme_flight", 30,       5
+  ) %>% 
+  crossing(binomial = c("Phocoena phocoena", 
+                        "Grampus griseus", 
+                        "Ziphius cavirostris", 
+                        "Physeter macrocephalus", 
+                        "Megaptera novaeangliae", 
+                        "Balaenoptera musculus"))
+
+thr_factory <- function(U_f_ms, tf_min, binom) {
+  function(td_min) {
+    # Morphologies
+    morph_row <- filter(morphologies, binomial == binom)
+    if (nrow(morph_row) != 1) stop("Imprecise binomial")
+    L_m = morph_row$Length_m
+    M_kg = morph_row$Mass_kg
     
-    # List of distribution function parameters
-    ff_flight <- fs_fun(data$U_f_ms, data$Length_m)
-    ff_basal <- fs_fun(U_b_ms, data$Length_m)
+    # Eout parameters (don't really matter, see sensitivity analysis)
+    ff_flight <- fs_fun(U_f_ms, L_m)
+    ff_basal <- fs_fun(U_b_ms, L_m)
     delta_ff <- ff_flight - ff_basal
-    ff_shape <- 4
-    ff_scale <- delta_ff / ff_shape
-    CL <- 1.46 + 0.0005 * data$Mass_kg
-    CL_shape <- 4
-    CL_scale <- CL / CL_shape
-    q_arg <- list(rf_h = list(),
-                  Ep_kJ = list(),
-                  delta_ff = list(shape = ff_shape, scale = ff_scale),
-                  CL = list(shape = CL_shape, scale = CL_scale))
+    CL <- 1.46 + 0.0005 * M_kg^2
     
-    param_args <- tibble(delta_ff = delta_ff,
-                         ff_shape = ff_shape,
-                         ff_scale = ff_scale,
-                         CL = CL,
-                         CL_shape = CL_shape,
-                         CL_scale = CL_scale)
+    # Ein parameters
+    rf_row <- filter(rf_tbl, binomial == binom)
+    if (nrow(rf_row) != 1) stop("Imprecise binomial")
+    rf_h <- rf_row$q_rf_fun[[1]](0.5)
     
-    # Plots of delta_ff, CL distributions
-    ggplot(data.frame(x = c(0, qgamma(0.99, 
-                                      shape = ff_shape, 
-                                      scale = ff_scale))), 
-           aes(x)) +
-      stat_function(fun = dgamma, 
-                    args = q_arg[[3]]) +
-      geom_vline(xintercept = delta_ff,
-                 linetype = "dashed") +
-      labs(x = "Change in fluking frequency (Hz)",
-           y = "Probability density", 
-           title = key$binomial) +
-      theme_minimal()
-    ggsave(sprintf("figs/ff_density/%s.pdf", key$binomial),
-           width = 9,
-           height = 6)
-    ggplot(data.frame(x = c(0, qgamma(0.99, 
-                                      shape = CL_shape, 
-                                      scale = CL_scale))), 
-           aes(x)) +
-      stat_function(fun = dgamma, 
-                    args = q_arg[[4]]) +
-      geom_vline(xintercept = CL,
-                 linetype = "dashed") +
-      labs(x = "Locomotor cost (J/kg/stroke)",
-           y = "density",
-           title = key$binomial) +
-      theme_minimal()
-    ggsave(sprintf("figs/ff_density/%s.pdf", key$binomial),
-           width = 9,
-           height = 6)
+    Ep_row <- filter(Ep_tbl, binomial == binom)
+    if (nrow(Ep_row) != 1) stop("Imprecise binomial")
+    Ep_kJ <- Ep_row$q_Ep_fun[[1]](0.5)
     
-    # Latin hypercube sample of parameter space
-    model <- Esonar_fun(data$t_d_min,
-                        data$t_f_min)
-    esonar_LHS <- pse::LHS(model, param, 1e3, q, q_arg)
+    # Model result
+    # Eout
+    Pout_W <- delta_ff * CL
+    Eout_kJ <- Pout_W / 1000 * tf_min * 60
+    # Ein
+    Pin_kJh <- rf_h * Ep_kJ
+    Pin_W <- Pin_kJh / 3600
+    Ein_kJ <- Pin_kJh * td_min / 60
     
-    sens_result <- esonar_LHS$data %>% 
-      mutate(Esonar_kJ = esonar_LHS$res[,1,1],
-             Eout_kJ = Eout_fun(data$t_f_min)(delta_ff, CL),
-             Ein_kJ = Ein_fun(data$t_d_min)(rf_h, Ep_kJ),
-             inout_ratio = Ein_kJ / Eout_kJ)
+    Esonar <- Eout_kJ + Ein_kJ
     
-    # ECDF of model outputs
-    esonar_ecdf <- ggplot(sens_result, aes(Esonar_kJ)) +
-      stat_ecdf() +
-      labs(x = "Energetic cost (kJ)",
-           y = "Cumulative probability",
-           title = key$binomial) +
-      theme_minimal()
+    # Daily FMR 
+    beta <- 3
+    daily_FMR <- beta * 293.1 * M_kg ^ 0.75
     
-    # Scatter of model outputs w.r.t. parameters
-    esonar_scatter <- sens_result %>% 
-      gather(parameter, value, rf_h:CL) %>% 
-      ggplot(aes(value, Esonar_kJ)) +
-      geom_point(size = 0.5) +
-      geom_smooth(method = "lm",
-                  se = FALSE) +
-      labs(x = "",
-           y = "Energetic cost (kJ)",
-           title = key$binomial) +
-      facet_wrap(~ parameter, 
-                 scales = "free_x",
-                 strip.position = "bottom") +
-      theme_minimal() +
-      theme(strip.placement = "outside")
-    
-    # Save plots
-    ggsave(sprintf("figs/esonar_ecdfs/%s.pdf", key$binomial),
-           esonar_ecdf,
-           width = 9,
-           height = 6)
-    ggsave(sprintf("figs/esonar_scatters/%s.pdf", key$binomial),
-           esonar_scatter,
-           width = 9,
-           height = 6)
-    
-    # Linear model results
-    esonar_linear <- sens_result %>%
-      # Normalize values using z-scores
-      mutate_at(vars(Esonar_kJ, rf_h, Ep_kJ, CL, delta_ff), 
-                function(x) (x - mean(x)) / sd(x)) %>% 
-      # Multiple regression
-      lm(Esonar_kJ ~ rf_h + Ep_kJ + CL + delta_ff, data = .)
-    
-    # Extract coefficients, p-values, and confidence intervals
-    esonar_coef <- coef(esonar_linear)
-    esonar_pval <- summary(esonar_linear)$coefficients[,4]
-    esonar_ci <- as_tibble(confint(esonar_linear, level = 0.95),
-                           rownames = "param")
-    colnames(esonar_ci)[2:3] <- c(ci_min, ci_max)
-    
-    # Combine and drop info for the intercept
-    lm_results <- cbind(esonar_ci, esonar_coef, esonar_pval)[-1,]
-    
-    esonar_results <- summarize(sens_result,
-                                mean_Esonar = mean(Esonar_kJ),
-                                median_Esonar = median(Esonar_kJ),
-                                iqr_Esonar = IQR(Esonar_kJ),
-                                median_inout = median(inout_ratio),
-                                inout_25 = quantile(inout_ratio, 0.25),
-                                inout_75 = quantile(inout_ratio, 0.75))
-    
-    cbind(lm_results, esonar_results, param_args)
-  })
+    abs(0 - (Esonar - daily_FMR))
+  }
+}
 
+find_thr <- function(thr_fun) {
+  optimize(thr_fun, lower = 0, upper = 31 * 24 * 60)$minimum
+}
 
-# A plot with the normalized linear model coefficients
-coef_data2 <- Esonar_tbl2 %>% 
-  mutate(param = factor(param, 
-                        levels = c("CL",
-                                   "delta_ff",
-                                   "Ep_kJ",
-                                   "rf_h"),
-                        labels = c("C[L]",
-                                   "Delta*f[f]",
-                                   "E[p]",
-                                   "r[f]")))
-ggplot(coef_data, aes(x = binomial, y = esonar_coef, group = binomial)) +
-  geom_hline(aes(yintercept = mean_coef),
-             summarize(group_by(coef_data, param), 
-                       mean_coef = mean(esonar_coef)),
-             linetype = "dashed") +
-  geom_point(position = position_dodge(width = 0.4)) +
-  geom_errorbar(aes(ymin = `2.5 %`, ymax = `97.5 %`), 
-                width = 0.2, 
-                position = position_dodge(width = 0.4)) +
-  facet_grid(~ param,
-             labeller = label_parsed,
-             switch = "x") +
-  labs(x = "Parameter",
-       y = "Normalized coefficient") +
-  theme_classic() +
-  theme(axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.title.x = element_blank(),
-        legend.position = "none",
-        strip.background = element_blank(),
-        strip.placement = "outside")
-
-ggsave("figs/extreme_flight_coefs.pdf",
-       width = 9,
-       height = 6)
+thr_tbl <- scenario_tbl %>% 
+  mutate(binomial = factor(binomial, levels = binom_levels),
+         thr_fun = pmap(list(U_f_ms, t_f_min, binomial), thr_factory),
+         td_min_thr = map_dbl(thr_fun, find_thr),
+         td_hr_thr = td_min_thr / 60,
+         td_day_thr = td_hr_thr / 24) %>% 
+  arrange(binomial, t_f_min)
